fe0ef5a81d8f5550bef6307605348ddcecc618d6
OKAYGEEE <Okayge17@gmail.com>
2024-11-05 03:00:30 -0300

    ROUTE(REPARACION)

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\LoginController;
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 
 
 
